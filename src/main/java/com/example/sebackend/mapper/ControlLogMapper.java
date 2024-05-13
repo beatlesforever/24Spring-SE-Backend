b33e4eda711afd2c420f592050b31a5c31eeb579
Lucy <2272013306@qq.com>
2024-05-13 14:25:16 +0800

    新增方法

--- src/main/java/com/example/sebackend/mapper/ControlLogMapper.java
+++ src/main/java/com/example/sebackend/mapper/ControlLogMapper.java
@@ -3,6 +3,10 @@ package com.example.sebackend.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.sebackend.entity.ControlLog;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * @author zhouhaoran
@@ -11,4 +15,9 @@ import org.apache.ibatis.annotations.Mapper;
  */
 @Mapper
 public interface ControlLogMapper extends BaseMapper<ControlLog> {
+    @Select("SELECT * FROM control_log WHERE roomId = #{roomId} AND isCompleted = false;")
+    ControlLog getUnfinishedLog(int roomId);
+
+    @Select("SELECT * FROM control_log WHERE roomId = #{roomId} AND isCompleted = true AND requestTime >= #{startTime} AND requestTime <= #{endTime};")
+    List<ControlLog> getFinishedLogs(int roomId, LocalDateTime startTime, LocalDateTime endTime);
 }
