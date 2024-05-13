0e7dcda2c28374f2d34dfea6873426949831366b
Lucy <2272013306@qq.com>
2024-05-13 14:29:20 +0800

    新增方法

--- src/main/java/com/example/sebackend/service/IUsageRecordService.java
+++ src/main/java/com/example/sebackend/service/IUsageRecordService.java
@@ -3,10 +3,14 @@ package com.example.sebackend.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.example.sebackend.entity.UsageRecord;
 
+import java.time.LocalDateTime;
+
 /**
  * @author zhouhaoran
  * @date 2024/5/8
  * @project SE-backend
  */
 public interface IUsageRecordService extends IService<UsageRecord> {
+    // 通过房间号获取在时间范围内的使用记录次数
+    int getUsageRecordCount(int roomId, LocalDateTime startTime, LocalDateTime endTime);
 }
