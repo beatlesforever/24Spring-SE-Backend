6722a982ee3688ae0470bf999047a1f604086c01
Lucy <2272013306@qq.com>
2024-05-13 14:28:44 +0800

    新增方法

--- src/main/java/com/example/sebackend/service/impl/UsageRecordServiceImpl.java
+++ src/main/java/com/example/sebackend/service/impl/UsageRecordServiceImpl.java
@@ -6,6 +6,9 @@ import com.example.sebackend.mapper.UsageRecordMapper;
 import com.example.sebackend.service.IUsageRecordService;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 /**
  * @author zhouhaoran
  * @date 2024/5/8
@@ -13,4 +16,13 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class UsageRecordServiceImpl extends ServiceImpl<UsageRecordMapper, UsageRecord> implements IUsageRecordService {
+    @Override
+    public int getUsageRecordCount(int roomId, LocalDateTime startTime, LocalDateTime endTime) {
+        List<UsageRecord> usageRecords = baseMapper.getUsageRecords(roomId, startTime, endTime);
+        if (usageRecords != null) {
+            return usageRecords.size();
+        } else {
+            return 0;
+        }
+    }
 }
