281cf44c3141fe68be6f66f5f61846da5a1796cb
Lucy <2272013306@qq.com>
2024-05-13 14:28:30 +0800

    新增方法

--- src/main/java/com/example/sebackend/service/impl/ControlLogServiceImpl.java
+++ src/main/java/com/example/sebackend/service/impl/ControlLogServiceImpl.java
@@ -7,11 +7,27 @@ import com.example.sebackend.mapper.CentralUnitMapper;
 import com.example.sebackend.mapper.ControlLogMapper;
 import com.example.sebackend.service.ICentralUnitService;
 import com.example.sebackend.service.IControlLogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * @author zhouhaoran
  * @date 2024/5/12
  * @project SE-backend
  */
+@Service
 public class ControlLogServiceImpl  extends ServiceImpl<ControlLogMapper, ControlLog> implements IControlLogService {
+
+    @Override
+    public ControlLog getUnfinishedLog(int roomId) {
+        return baseMapper.getUnfinishedLog(roomId);
+    }
+
+    @Override
+    public List<ControlLog> getFinishedLogs(int roomId, LocalDateTime startTime, LocalDateTime endTime) {
+        return  baseMapper.getFinishedLogs(roomId, startTime, endTime);
+    }
 }
