a11ed0908b6232d4f9a1e2692fb8d8c443f726ae
zhachenyu <1391650978@qq.com>
2024-05-14 22:00:20 +0800

    添加了用户名重复导致的注册失败认证

--- src/main/resources/static/registration.js
+++ src/main/resources/static/registration.js
@@ -1,34 +1,40 @@
 document.getElementById("registerForm").addEventListener("submit", function(event) {
-   event.preventDefault();
-   // 获取表单数据
-   var username = document.getElementById("username").value;
-   var password = document.getElementById("password").value;
-   var roomId = document.getElementById("roomId").value;
-   var role = document.getElementById("role").value;
-   
-   // 构建请求体数据
-   var data = {
-       username: username,
-       password: password,
-       roomId: roomId,
-       role: role
-   };
-   
-   // 发送 POST 请求到后端
-   fetch('/api/users/register', {
-       method: 'POST',
-       headers: {
-           'Content-Type': 'application/json'
-       },
-       body: JSON.stringify(data)
-   })
-   .then(response => response.json())
-   .then(data => {
-       console.log('Registration response:', data);
-       window.location.href = 'registration.html';
-       // 在这里处理注册响应，例如显示成功消息或重定向到登录页面
-   })
-   .catch(error => {
-       console.error('Error:', error);
-   });
+    event.preventDefault();
+    // 获取表单数据
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    var roomId = document.getElementById("roomId").value;
+    var role = document.getElementById("role").value;
+
+    // 构建请求体数据
+    var data = {
+        username: username,
+        password: password,
+        roomId: roomId,
+        role: role
+    };
+
+    // 发送 POST 请求到后端
+    fetch('/api/users/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+        .then(response => response.json())
+        .then(data => {
+            if (data.message === "用户注册成功") {
+                console.log('Registration response:', data);
+                window.location.href = 'registration.html';
+                // 注册成功消息
+                alert("注册成功");
+            } else {
+                // 注册失败消息
+                alert("注册失败，用户名可能已存在");
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
 });
