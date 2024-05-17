da2977bfb2102e0b45041b02fb52c84a5d8b10a9
zhachenyu <1391650978@qq.com>
2024-05-17 12:17:08 +0800

    添加了跨域配置，现在可以做到前后端的交互了

--- src/main/java/com/example/sebackend/config/SecurityConfig.java
+++ src/main/java/com/example/sebackend/config/SecurityConfig.java
@@ -6,6 +6,12 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 /**
  * @author zhouhaoran
@@ -29,12 +35,25 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         // 禁用跨站请求伪造（CSRF）保护
         http.csrf().disable()
+                .cors()
+                .and()
                 // 对静态资源、登录和注册页面的访问进行许可，无需认证即可访问
-                .authorizeRequests().antMatchers("/*.html", "/*.css","/*.js","/login", "/home","/api/users/login", "/api/users/register").permitAll()
+                .authorizeRequests().antMatchers("/api/users/login", "/api/users/register").permitAll()
                 // 对其他所有请求进行认证，即只有经过认证的用户才能访问
                 .anyRequest().authenticated()
                 .and()
                 // 在UsernamePasswordAuthenticationFilter之前添加自定义的JWT请求过滤器
                 .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
