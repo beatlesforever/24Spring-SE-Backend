dcf2239c9c404bda1bb7f23f9388030575a70ba6
Lucy <2272013306@qq.com>
2024-05-18 16:40:22 +0800

    修改sql语句

--- src/main/java/com/example/sebackend/mapper/ControlLogMapper.java
+++ src/main/java/com/example/sebackend/mapper/ControlLogMapper.java
@@ -3,6 +3,7 @@ package com.example.sebackend.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.sebackend.entity.ControlLog;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 import java.time.LocalDateTime;
@@ -15,13 +16,11 @@ import java.util.List;
  */
 @Mapper
 public interface ControlLogMapper extends BaseMapper<ControlLog> {
-    @Select("SELECT * FROM control_log WHERE room_id = #{roomId} AND is_completed = false;")
+    @Select("SELECT * FROM control_log WHERE room_id = #{roomId} AND is_completed = 0;")
     ControlLog getUnfinishedLog(int roomId);
 
-    @Select("SELECT * FROM control_log WHERE room_id = #{roomId} AND is_completed = true AND request_time >= #{startTime} AND request_time <= #{endTime};")
-    List<ControlLog> getFinishedLogs(int roomId, LocalDateTime startTime, LocalDateTime endTime);
+    @Select("SELECT * FROM control_log WHERE room_id = #{roomId} AND is_completed = 1 AND request_time >= #{startTime} AND request_time <= #{endTime};")
+    List<ControlLog> getFinishedLogs(@Param("roomId") int roomId,@Param("startTime") LocalDateTime startTime,@Param("endTime") LocalDateTime endTime);
 
-    //将所有的房间号相同的记录按照时间排序，取最新的一条
-    @Select("SELECT * FROM control_log WHERE room_id = #{roomId} ORDER BY request_time DESC LIMIT 1;")
     ControlLog getLatestLog(int roomId);
 }
