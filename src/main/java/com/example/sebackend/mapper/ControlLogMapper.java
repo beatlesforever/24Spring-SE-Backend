cfe07ec0e4b7ce5e302b6db25a886bafa8ef3785
sunny <3185559784@qq.com>
2024-05-18 09:53:00 +0800

    添加记录,control_log和usage_record

--- src/main/java/com/example/sebackend/mapper/ControlLogMapper.java
+++ src/main/java/com/example/sebackend/mapper/ControlLogMapper.java
@@ -15,10 +15,10 @@ import java.util.List;
  */
 @Mapper
 public interface ControlLogMapper extends BaseMapper<ControlLog> {
-    @Select("SELECT * FROM control_log WHERE roomId = #{roomId} AND isCompleted = false;")
+    @Select("SELECT * FROM control_log WHERE room_id = #{roomId} AND is_completed = false;")
     ControlLog getUnfinishedLog(int roomId);
 
-    @Select("SELECT * FROM control_log WHERE roomId = #{roomId} AND isCompleted = true AND requestTime >= #{startTime} AND requestTime <= #{endTime};")
+    @Select("SELECT * FROM control_log WHERE room_id = #{roomId} AND is_completed = true AND request_time >= #{startTime} AND request_time <= #{endTime};")
     List<ControlLog> getFinishedLogs(int roomId, LocalDateTime startTime, LocalDateTime endTime);
 
     //将所有的房间号相同的记录按照时间排序，取最新的一条
