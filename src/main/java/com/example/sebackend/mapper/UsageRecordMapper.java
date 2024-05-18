17430ec04d31f5aa413ce33bb22fa54979c32806
Lucy <2272013306@qq.com>
2024-05-18 16:40:39 +0800

    修改sql语句

--- src/main/java/com/example/sebackend/mapper/UsageRecordMapper.java
+++ src/main/java/com/example/sebackend/mapper/UsageRecordMapper.java
@@ -3,6 +3,7 @@ package com.example.sebackend.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.sebackend.entity.UsageRecord;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 import java.time.LocalDateTime;
@@ -18,7 +19,7 @@ public interface UsageRecordMapper extends BaseMapper<UsageRecord> {
 
     //通过房间号查询时间范围内的使用记录
     @Select("SELECT * FROM usage_record WHERE room_id = #{roomId} AND start_time >= #{startTime} AND end_time <= #{endTime};")
-    List<UsageRecord> getUsageRecords(int roomId, LocalDateTime startTime, LocalDateTime endTime);
+    List<UsageRecord> getUsageRecords(@Param("roomId") int roomId,@Param("startTime") LocalDateTime startTime,@Param("endTime") LocalDateTime endTime);
 
     //按照房间号查找所有房间号相同的记录
     List<UsageRecord> saveEndRecord(int roomId);
