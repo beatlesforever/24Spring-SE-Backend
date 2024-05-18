fa83a7d5e6b09670c6df863ce67ad2e521ced5be
Lucy <2272013306@qq.com>
2024-05-18 16:39:56 +0800

    修改接口

--- src/main/java/com/example/sebackend/controller/ReportController.java
+++ src/main/java/com/example/sebackend/controller/ReportController.java
@@ -7,13 +7,12 @@ import com.example.sebackend.service.IControlLogService;
 import com.example.sebackend.service.IReportService;
 import com.example.sebackend.service.IRoomService;
 import com.example.sebackend.service.IUsageRecordService;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
@@ -26,6 +25,7 @@ import java.util.Map;
  * @project SE-backend
  */
 @RestController
+@Slf4j
 @RequestMapping("/api/reports")
 public class ReportController {
     @Autowired
@@ -53,18 +53,21 @@ public class ReportController {
      * 2.查询时间范围内的温控请求时，以请求开始的时间为准，即若结束时间在查询时间范围外，也包含在本报表内。
      *
      * @param roomId 房间ID
-     * @param queryTime 查询时间
+     * //@param queryTime 查询时间
      * @param reportType 报告类型
      * @return ResponseEntity<Map<String, Object>> 包含更新后的房间信息的HTTP响应实体，
      * 其中状态码为OK（200），信息为"实时房间能耗费用查询成功"，数据部分为累积能耗和累积费用。
      */
 
-    @GetMapping("/{roomId}{queryTime}{reportType}")
-    public ResponseEntity<Map<String, Object>> getReport(@PathVariable int roomId, @PathVariable LocalDateTime queryTime, @PathVariable String reportType) {
+    @GetMapping("/getReport")
+    public ResponseEntity<Map<String, Object>> getReport(@RequestParam int roomId, @RequestParam String reportType) {
+        log.info("room id: " + roomId + " report type: " + reportType );
         Report report = new Report();
         // 通过roomId查询房间信息
         Room room = roomService.getById(roomId);
         LocalDateTime startTime = null;
+        // 获取当前时间
+        LocalDateTime queryTime = LocalDateTime.now();
         // 确定查找类型
         if(reportType.equals("daily")){
             // 如果是日报表，查询开始时间为当天0点
@@ -79,12 +82,11 @@ public class ReportController {
             startTime = queryTime.withDayOfMonth(1).toLocalDate().atStartOfDay();
         }
         // 查询结束时间为当前请求时间
-        LocalDateTime endTime = queryTime;
 
         // 查询时间范围内的该房间内的已经完成的温控请求
-        List<ControlLog> controlLogs = controlLogService.getFinishedLogs(roomId, startTime, endTime);
+        List<ControlLog> controlLogs = controlLogService.getFinishedLogs(roomId, startTime, queryTime);
         // 查询时间范围内的该房间内的从控机使用记录次数
-        int count = usageRecordService.getUsageRecordCount(roomId, startTime, endTime);
+        int count = usageRecordService.getUsageRecordCount(roomId, startTime, queryTime);
 
         // 累加报告期间总能量消耗和总费用
         float totalEnergyConsumed = 0.0f;
@@ -94,7 +96,7 @@ public class ReportController {
             totalCost += controlLog.getCost();
         }
 
-        //将从控机使用记录次数、每个温控请求起止时间、温控请求的起止温度及风量消耗大小、每次温控请求所需费用转换成String形式
+        // 将从控机使用记录次数、每个温控请求起止时间温控请求的起止温度及风量消耗大小、每次温控请求所需费用转换成String形式
         String details = "从控机使用记录次数："+count+"\n";
         for(ControlLog controlLog : controlLogs){
             details += "温控请求起止时间：" + controlLog.getRequestTime() + "至" + controlLog.getEndTime() + "\n";
