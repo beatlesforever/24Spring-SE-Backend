3371f08c4b7e0f5b3476d1367b1541f12bdf00e3
Lucy <2272013306@qq.com>
2024-05-22 13:17:02 +0800

    报表属性修改

--- src/main/java/com/example/sebackend/entity/Report.java
+++ src/main/java/com/example/sebackend/entity/Report.java
@@ -12,6 +12,7 @@ import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * Report 实体类用于生成不同类型的报告，如日报、周报、月报。
@@ -27,6 +28,7 @@ public class Report {
     private LocalDateTime generationDate;  // 报告生成日期
     private Float totalEnergyConsumed;  // 报告期间总能量消耗
     private Float totalCost;  // 报告期间总费用
-    private String details;  // 报告的详细内容
+    private Integer usageTime;  // 从控机使用次数
+    private List<TempLog> log;// 温控请求具体内容
     private String creator;  // 报告生成者
 }
