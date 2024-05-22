7f78958db3329c3845b901cd01f58598af737657
Lucy <2272013306@qq.com>
2024-05-22 13:17:24 +0800

    新的entity，用于构造返回的数组

---
+++ src/main/java/com/example/sebackend/entity/TempLog.java
@@ -0,0 +1,15 @@
+package com.example.sebackend.entity;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class TempLog {
+    private Float actualTemp;  // 实际温度
+    private Float endTemp;  // 结束温度
+    private String requestedFanSpeed;  // 请求的风速 ('high', 'medium', 'low')
+    private LocalDateTime requestTime;  // 请求时间
+    private LocalDateTime endTime;  // 调节结束时间，系统完成调节的时间
+    private Float cost; //所需费用
+}
