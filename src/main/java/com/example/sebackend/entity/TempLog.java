2b5176a48d808b1d14561d72da1dca9a9e5de18d
Lucy <2272013306@qq.com>
2024-05-29 18:59:51 +0800

    新增duration和能耗字段

--- src/main/java/com/example/sebackend/entity/TempLog.java
+++ src/main/java/com/example/sebackend/entity/TempLog.java
@@ -11,5 +11,7 @@ public class TempLog {
     private String requestedFanSpeed;  // 请求的风速 ('high', 'medium', 'low')
     private LocalDateTime requestTime;  // 请求时间
     private LocalDateTime endTime;  // 调节结束时间，系统完成调节的时间
+    private Integer duration;//持续时间
+    private Float energyConsumed; //能耗
     private Float cost; //所需费用
 }
